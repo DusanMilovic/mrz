d3b3b1179a74f579ef8dac90d715f870cae6b461
Dusan Milovic <Dusan95kv@gmail.com>
2019-08-24 11:13:24 +0200

    Add files via upload

---
+++ datagramPaket.java
@@ -0,0 +1,21 @@
+publi class datagram {
+	public static void main(String[] args) throw SocketException {
+		String s = "Домовина се брани лепотом, и чашћу и знањем. Домовина се брани животом и лепим васпитањем."
+		try {
+			// u DATAGRAM mozemo da trpamo samo bajtove, ne string
+			byte[] data = s.getBytes("UTF-8");
+			try (DatagramSocket socket = new DatagramSocket(0)) {
+				InetAddress ia = InetAddress.getByName("192.168.8.255");
+				int port = 1056;
+				DatagramPacket datagramPaket = new DatagramPacket(data, data.length, ia, port);
+				socket.send(datagramPaket);
+			} catch (UnknownHostException uex) {
+				System.err.println(uex);
+			} catch (IOException ioex) {
+				System.err.println(ioex);
+			}
+		} catch (UnsupportedEncodingException err) {
+			System.err.println(err);
+		}
+	}
+}
